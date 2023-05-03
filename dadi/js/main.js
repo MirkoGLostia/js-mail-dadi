eb0e4b2c49c0732da5f5dff2da95da8a9e99a67e
MirkoGLostia <giggi.torre@mail.com>
2023-05-03 18:25:23 +0200

    js_interaction-complete

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -4,40 +4,47 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto
 */
 
 // variabili
+let user, pc, result;
+
+// ricevere input bottone dello user
+submit.onclick = function () {
+    
+    // generare numero random per user e pc
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+      
+      user = parseInt(randomIntFromInterval(1, 6));
+      pc = parseInt(randomIntFromInterval(1, 6));
 
 
-// ricevere input bottone del player
+      // output risultati
+      document.getElementById("user-dice").innerText = user;
+      document.getElementById("pc-dice").innerText = pc;
 
 
-// generare numero random per player
 
 
-    // output
+      // fare la differenza (player - pc)
+      result = (user - pc);
+      
+      
+      if (result > 0) {
+         // se valore positivo vince player
+         document.getElementById("result-of-roll").innerText = "User wins";
+
+      } 
+      else if (result < 0) {
+        // se valore negativo vince pc
+        document.getElementById("result-of-roll").innerText = "Pc wins";
+
+      }
+      else {
+        // altrimenti patta
+        document.getElementById("result-of-roll").innerText = "Sport wins";
+
+      }
+}
 
 
-// generare numero random per pc
-
-
-    // output
-
-
-// fare la differenza (player - pc)
-
-
-    // se valore positivo vince player 
-
-
-        // output
-
-
-    // se valore negativo vince pc
-
-
-        // output
-
-
-    // altrimenti patta
-
-
-        // output
 
