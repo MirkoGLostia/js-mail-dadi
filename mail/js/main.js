3680ab3c124ed194a1e65d032fdf0bba0a2f55e8
MirkoGLostia <giggi.torre@mail.com>
2023-05-03 13:34:33 +0200

    js_logica-mail

---
+++ mail/js/main.js
@@ -0,0 +1,25 @@
+/*
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+QUI ricordatevi che non è concesso usare altro oltre quello che abbiam visto oggi.
+*/
+
+
+// variabili e array
+
+
+// ricezione dati da input bottone
+
+
+// ciclare array
+
+
+    // confronto dati input con dati array
+
+
+        // se c'è corrispondeza output messaggio nella pagina
+
+
+        // se non c'è corrispondenza output alert
+
+
+
