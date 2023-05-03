5ca31d84ad4b9f19d0609cc23dea0f5b15c08ffa
MirkoGLostia <giggi.torre@mail.com>
2023-05-03 13:29:28 +0200

    js_logica-dadi

---
+++ dadi/js/main.js
@@ -0,0 +1,43 @@
+/*
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto
+*/
+
+// variabili
+
+
+// ricevere input bottone del player
+
+
+// generare numero random per player
+
+
+    // output
+
+
+// generare numero random per pc
+
+
+    // output
+
+
+// fare la differenza (player - pc)
+
+
+    // se valore positivo vince player 
+
+
+        // output
+
+
+    // se valore negativo vince pc
+
+
+        // output
+
+
+    // altrimenti patta
+
+
+        // output
+
